88b6e849bd1402597b24c63c97121c662cc5b24f
raul <zarater.raul@gmail.com>
2022-03-27 09:10:01 -0700

    added some comments and some html layout

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+here is code that will:
+ make things smooth
+ create card questions
+ create random generator
+ create bonus round
+ create options for answers
+ create questions
+ tally points
+
+*/
